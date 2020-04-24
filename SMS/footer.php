f3fe5f45b1963570e81b3cb26d4ee7f50909c0ca
viki-84835 <56757618+viki-84835@users.noreply.github.com>
2020-04-25 00:51:56 +0530

    Update footer.php

--- SMS/footer.php
+++ SMS/footer.php
@@ -3,7 +3,7 @@
         <footer class="footer">
           <div class="container-fluid clearfix">
             <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">
-              <span style="color: #424964">My11Student   </span>   School Management System</span>
+              <span style="color: #424964">My11Student   </span>   Student Management System</span>
           </div>
         </footer>
         <!-- partial -->'
