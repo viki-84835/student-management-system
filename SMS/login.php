661121d95a69b690647d465ed7210ce85209809d
viki-84835 <56757618+viki-84835@users.noreply.github.com>
2020-04-25 00:51:30 +0530

    Update login.php

--- SMS/login.php
+++ SMS/login.php
@@ -143,7 +143,7 @@
 								</form>
 							</div>
 							<br/>
-							<p class="footer-text text-center">My11Student<span style="color: #AA3339; font-weight: bold">School Management System</span> </p>
+							<p class="footer-text text-center">My11Student<span style="color: #AA3339; font-weight: bold">student Management System</span> </p>
 						</div>
 					</div>
 				</div>
